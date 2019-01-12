ecf9ef556be5632c26e17805c935b65fa4a24eb6
MadhavGajalwar <madhavgajalwar123@gmail.com>
2019-01-12 19:55:29 +0530

    array example
    array using c sharp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[] arr = new int[4];
+            string[] array = new string[3];
+
+            arr[0] = 0;
+            arr[1] = 10;
+            arr[3] = 100;
+            arr[2] = 125;
+            array[0] = "Madhav";
+            array[1] = "Gajalwar";
+            array[2] = "XYZ";
+            Console.Write(arr[2] );
+            Console.Write(array[0]);
+            Console.ReadLine();
+
+        }
+    }
+}
